dc73b18294080a6fa6d1103c80bc7e62833dc836
Lorans <elornorthwind@gmail.com>
2023-05-10 23:35:42 +0300

    max-width variant

--- src/shared/ui/SectionWrapper/SectionWrapper.tsx
+++ src/shared/ui/SectionWrapper/SectionWrapper.tsx
@@ -8,8 +8,8 @@ interface SectionWrapperProps {
 export default function SectionWrapper(props: SectionWrapperProps) {
   const { children, className } = props;
   return (
-    <div className={`h-screen w-full bg-stone-200 text-slate-600 snap-start snap-always overflow-hidden ${className}`}>
-      {/* <div className=" relative max-w-7xl mx-auto h-full bg-stone-200">{children}</div> */}
+    <div className={`relative h-screen w-full  text-slate-600 snap-start snap-always overflow-hidden ${className}`}>
+      {/* <div className="relative max-w-7xl mx-auto h-full bg-[#f7f7f0] shadow-2xl">{children}</div> */}
       {children}
     </div>
   );
