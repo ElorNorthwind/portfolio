83bd5f1bd27186762046d3e1808ac3d20aa4beab
Lorans <elornorthwind@gmail.com>
2023-05-10 16:21:28 +0300

    vite config base

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import Section1 from "@/pages/Section1/Section1";
 import Section2 from "@/pages/Section2/Section2";
+import Section3 from "@/pages/Section3/Section3";
 
 function App() {
   return (
     <div className="snap-y snap-mandatory">
       <Section1 />
       <Section2 />
+      <Section3 />
     </div>
   );
 }
