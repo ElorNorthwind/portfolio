b77288df644a033c35acb53f0290691678ff9479
Lorans <elornorthwind@gmail.com>
2023-05-11 10:46:41 +0300

    navbar styling

--- src/widgets/Navbar/Navbar.tsx
+++ src/widgets/Navbar/Navbar.tsx
@@ -1,21 +1,15 @@
-import { MutableRefObject } from "react";
-
 interface NavbarProps {
   refs: (HTMLDivElement | null)[];
 }
 export default function Navbar(props: NavbarProps) {
   const { refs } = props;
 
-  //   if (!refs?.current) {
-  //     return null;
-  //   }
-
   return (
-    <div className={`fixed left-3 top-1/2 -translate-y-1/2 gap-2 flex flex-col`}>
+    <div className={`fixed left-3 top-1/2 -translate-y-1/2 gap-3 flex flex-col`}>
       {refs.length > 0 &&
         refs.map((ref) => (
           <div
-            className={`rounded-full bg-black bg-opacity-20 h-3 w-3 cursor-pointer hover:bg-opacity-30`}
+            className={`rotate-45 bg-black bg-opacity-20 h-3 w-3 cursor-pointer hover:bg-opacity-30`}
             onClick={() => ref?.scrollIntoView({ behavior: "smooth" })}
           ></div>
         ))}
